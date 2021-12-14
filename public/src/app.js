87fa6b2f1e8ab0f2f3cfbf9658a96296d6774fc1
Meiarasu K <95343759+K-Meiarasu@users.noreply.github.com>
2021-12-14 15:54:57 +0530

    Update app.js

--- public/src/app.js
+++ public/src/app.js
@@ -24,3 +24,64 @@ function removeChocolates(chocolates,count)
     chocolates=c1;
     return chocolates;
 }
+function dispenseChocolates(chocolates,n)
+{
+    var modify=[]
+    for(let i=chocolates.length-n,j=0;i<chocolates.length;i++,j++)
+    {
+        modify[j]=chocolates[i]
+    }
+    return modify
+}
+function dispenseChocolatesOfColor(chocolates,count,color)
+{
+    var modified=[]
+    for(let i=chocolates.length-1;i>=0;i--)
+    {
+        if(chocolates[i]==color && count!=0)
+        {
+            modified.push(chocolates[i])
+            chocolates.splice(i,1)
+            count--
+        }
+    }
+    return modified
+}
+function noOfChocolates(chocolates)
+{
+    var  num=[];
+    var array=['green', 'silver', 'blue', 'crimson', 'purple', 'red', 'pink'];
+    for(let i=0;i<array.length;i++)
+    {
+       
+        var count=0;
+        for(let j=0;j<chocolates.length;j++)
+        {
+            if(array[i]==chocolates[j])
+            {
+                count++;
+            }
+        }
+        num.push(count);
+    }
+    return num;
+}
+function changeChocolateColor(chocolates,n,color,finalColor){
+    for(let i=0;i<n;i++){
+             chocolates[chocolates.indexOf(color)]=finalColor;
+    }
+    return chocolates;
+}
+function changeChocolateColorAllofxCount(chocolates,finalColor,color)
+{
+    var count=0
+    for(let i=chocolates.length-1;i>=0;i--)
+    {
+        if(chocolates[i]==color)
+        {
+            chocolates[i]=finalColor
+            count++
+        }
+    }
+    return [chocolates,count]
+}
